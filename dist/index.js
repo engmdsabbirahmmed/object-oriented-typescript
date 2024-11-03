d9531921ddf92a2eef9588752183570ec301ba34
Sabbir Ahmmed <engineersabbir.dev@gmail.com>
2024-11-03 22:23:11 +0600

    class and object file added

--- dist/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-const myName = 'Md. sabbir ahmmed';
---
+++ src/class-and-object.ts
@@ -0,0 +1,47 @@
+{
+    //class-and-object
+    // class Animal {
+    //     name: string;
+    //     species: string;
+    //     sound: string;
+    //     price: number;
+
+    //     constructor(name: string, species: string, sound: string, price: number) {
+    //         this.name = name;
+    //         this.species = species;
+    //         this.sound = sound;
+    //         this.price = price;
+    //     }
+
+    //     makeSound() {
+    //         console.log(`This ${this.name} says ${this.sound}`)
+    //     }
+    // }
+
+
+
+    //using parameter:=>
+    class Animal {
+        constructor(public name: string,public species: string,public sound: string, public price: number | string) {}
+
+        makeSound() {
+            console.log(`This ${this.name} says ${this.sound}`)
+        }
+        findPrice(){
+            console.log(`This ${this.name}'s price is ${this.price}`)
+        }
+    }
+
+    const dog = new Animal('Bangladeshi Dog', 'dog', 'ghew ghew', 0);
+    const cat = new Animal('Indian Pussy Cat', 'cat', 'meow meow', 700);
+    const tiger = new Animal('Tiger mama', 'Bengal tiger', 'halum halum', 'infinity')
+
+    cat.makeSound();
+    dog.makeSound();
+    tiger.makeSound();
+    tiger.findPrice()
+
+
+    //
+
+}
